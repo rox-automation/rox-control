fff566a5101b712e1577c7f9fe2354baf9f4e24f
Jev <jev.kuznetsov@gmail.com>
2025-06-15 14:06:14 +0200

    update test imports to suppress linting warnings

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -3,8 +3,8 @@ import rox_control
 
 def test_imports() -> None:
     """Test that the main modules can be imported without errors."""
-    import rox_control
-    import tools.bycicle_model
+    import rox_control  # noqa: F401
+    import tools.bycicle_model  # noqa: F401
 
 
 def test_version() -> None:
